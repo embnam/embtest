d72482653a8943ee7ef6afea1d610cb1dbf9c22d
embnam <378495481@qq.com>
2026-03-03 17:59:22 +0800

    last commit

--- test.c
+++ test.c
@@ -8,5 +8,6 @@ int main() {
     greet();
     printf("This is a test program using C17 standard.\n");
     printf("C17 is the latest standard for the C programming language.\n");
+    printf("It includes various improvements and bug fixes over previous standards.\n");
     return 0;
 }
